aa0a0af22f2474f58dabb82a3d2cb6f30a3b51dc
owenlieyanto <62794634+owenlieyanto@users.noreply.github.com>
2022-09-21 15:14:03 +0700

    text input UPC fixed

---
+++ Part_A_Rails_Funproject/app/javascript/application.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "custom/main"
+
+// https://stackoverflow.com/questions/70548841/how-to-add-custom-js-file-to-new-rails-7-project
